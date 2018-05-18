a4cbe71556b7e39c4c2defbb81f3fc4cd1940e55
Berina Dervisic <dervisicberina@hotmail.com>
2018-05-19 01:15:20 +0200

    DIP-9 Make a list/menu with the list of channels *implemented player *implemented displaying current channel in the footer *added channel logo and play icon

--- modules/users/client/controllers/channels/channel.list.controller.js
+++ modules/users/client/controllers/channels/channel.list.controller.js
@@ -1,24 +1,30 @@
 (function () {
     'use strict';
-  
+
     angular
       .module('users')
       .controller('ChannelListController', ChannelListController);
-  
+
       ChannelListController.$inject = ['$scope','ChannelsService','Authentication'];
-  
+
     function ChannelListController($scope,ChannelsService,Authentication) {
-      
+
       var vm = this;
       vm.channelList = getChannels();
-      vm.user = Authentication.user; 
-      console.log("lista"); 
-      
+      vm.user = Authentication.user;
+      vm.currentChannel = "nesto";
+      vm.changeCurrentChannel = changeCurrentChannel;
+      console.log("lista");
+
+      function changeCurrentChannel(channel) {
+        vm.currentChannel = channel.name;
+        console.log("nestooo");
+      }
+
       function getChannels(){
         return ChannelsService.loadChannels();
       }
     }
 
-    
+
   }());
-  
